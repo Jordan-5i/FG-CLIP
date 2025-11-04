81b98d4ca74ce1682420485676d7e801fa8d138f
Jordan-5i <790712086@qq.com>
2025-11-04 15:54:01 +0800

    axmodel

--- ax_tools/run_axmodel.py
+++ ax_tools/run_axmodel.py
@@ -1,13 +1,11 @@
 import numpy as np
 from PIL import Image
-import onnxruntime as ort
+import axengine as ort
 import torch
 import os
-import tarfile
 from transformers import (
     AutoImageProcessor,
     AutoTokenizer,
-    AutoModelForCausalLM,
 )
 
 
@@ -25,42 +23,13 @@ def determine_max_value(image):
     else:
         return 128
 
-
-def prepare_calib():
-    calib_dir = "calib_data"
-    if not os.path.exists(calib_dir):
-        os.makedirs(calib_dir)
-    image_sub_dir = os.path.join(calib_dir, "image")
-    text_sub_dir = os.path.join(calib_dir, "text")
-    if not os.path.exists(image_sub_dir):
-        os.makedirs(image_sub_dir)
-    if not os.path.exists(text_sub_dir):
-        os.makedirs(text_sub_dir)
-        
-    image_tar = tarfile.open(os.path.join(image_sub_dir, "calib_data_image.tar"), "w")
-    text_tar = tarfile.open(os.path.join(text_sub_dir, "calib_data_text.tar"), "w")
-    
-    np.save(os.path.join(image_sub_dir, "image_input.npy"), 
-            {"pixel_values": image_input["pixel_values"].numpy().astype(np.float32), 
-            "pixel_attention_mask": image_input["pixel_attention_mask"].numpy().astype(np.int32)})
-    image_tar.add(os.path.join(image_sub_dir, "image_input.npy"))
-    
-    for i, c in enumerate(caption_input["input_ids"]):
-        np.save(os.path.join(text_sub_dir, f"input_ids_{i}.npy"), {"input_ids": c[None].numpy().astype(np.int64)})
-        text_tar.add(os.path.join(text_sub_dir, f"input_ids_{i}.npy"))
-    
-    text_tar.close()
-    image_tar.close()
-    
-    return
-
     
 if __name__ == "__main__":
     image_path = "bedroom.jpg"
-    model_root = "/data/wangjian/project/hf_cache/qihoo360/fg-clip2-base"
+    model_root = "/root/wangjian/hf_cache/fg-clip2-base"
     
-    image_encoder_path = "image_encoder.onnx"
-    text_encoder_path = "text_encoder.onnx"
+    image_encoder_path = "image_encoder.axmodel"
+    text_encoder_path = "text_encoder.axmodel"
     
     onnx_image_encoder = ort.InferenceSession(image_encoder_path)
     onnx_text_encoder = ort.InferenceSession(text_encoder_path)
@@ -89,7 +58,7 @@ if __name__ == "__main__":
     text_feature = []
     for c in caption_input["input_ids"]:
         tmp_text_feature = onnx_text_encoder.run(None, {
-            "input_ids": c[None].numpy().astype(np.int64),
+            "input_ids": c[None].numpy().astype(np.int32),
         })[0]
         text_feature.append(tmp_text_feature)
     text_feature = np.concatenate(text_feature, axis=0)
@@ -100,4 +69,3 @@ if __name__ == "__main__":
 
     print("Logits per image:", torch.from_numpy(logits_per_image).softmax(dim=-1))
     
-    prepare_calib()
